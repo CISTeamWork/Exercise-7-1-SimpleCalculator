f372a6c911bf1ce1d18e1b1db0b155db40dff155
George-Gachoki <gachokigeorge@yahoo.com>
2020-05-12 05:46:58 -0700

    update 05/13

--- SimpleCalculator/Form1.cs
+++ SimpleCalculator/Form1.cs
@@ -38,38 +38,41 @@ namespace SimpleCalculator
                 txtOperand1.Focus();
             }
             //catch block that handles FormatException
-            catch (FormatException ex)
+            catch (FormatException )
             {
                 //message box shows non-numeric entry error
                 MessageBox.Show("Format error. Please enter numeric values for operands", "Entry Error");
                 //message box shows entry error stack trace
-                MessageBox.Show("Format error. Please enter numeric values for operands" + "\n\n" + ex.StackTrace, "Entry Error");
+                //MessageBox.Show("Format error. Please enter numeric values for operands" + "\n\n" + ex.StackTrace, "Entry Error");
             }
             //catch block that handles OverflowException
-            catch (OverflowException ex)
+            catch (OverflowException )
             {
                 //message box shows overflow exception
-                MessageBox.Show("Overflow error. Please enter a valid operator.", "Entry Error");
-                
+                MessageBox.Show("Overflow error. Please enter smaller Operand1 and/or larger Operand2.", "Memory Overflow Error");
+
             }
             //catch block that handles DivisionByZeroException
-            catch (DivideByZeroException ex)
+            catch (DivideByZeroException )
             {
                 //message box shows division by zero error
-                MessageBox.Show("Division-by-zero error. Please enter a none zero value for operand2.", "Entry Error");
+                MessageBox.Show("Division-by-Zero error. Please enter a non-zero value for operand2.", "Division by 0 Error");
                
             }
-            //catch block that handles any type of exceptions
+            //catch block that handles any  type of uncatched exceptions
             catch (Exception ex)
             {
                 //message box shows error when any other type of exception occurs
-                MessageBox.Show("Invalid input for operands." + "\n\n" + ex.StackTrace, "Entry Error");
+                MessageBox.Show(ex.Message + "\n\n" + ex.GetType().ToString() + "\n" +
+             ex.StackTrace, "Unexpexted Runtime Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
             }
         }
         private decimal Calculate(decimal operand1, string operator1,
             decimal operand2)
         {
-            decimal result = 0;
+            decimal result =  0;
+
             if (operator1 == "+")
                 result = operand1 + operand2;
             else if (operator1 == "-")
@@ -78,15 +81,16 @@ namespace SimpleCalculator
                 result = operand1 * operand2;
             else if (operator1 == "/")
                 result = operand1 / operand2;
+            else if (operator1 == "**" || operator1 == "^")
+                result = (decimal)Math.Pow((double)operand1, (double)operand2);
             else
-                throw new OverflowException();
+                throw new Exception("Unknown Operator1!");
             return result;
         }
         private void btnExit_Click(object sender, EventArgs e)
         {
             this.Close();
         }
-
         private void ClearResult(object sender, EventArgs e)
         {
             this.txtResult.Text = "";
